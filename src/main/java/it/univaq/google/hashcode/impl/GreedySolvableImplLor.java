73518eab82d97739acd22077e024f990c68482d3
lollix91 <lollix@live.it>
2018-03-01 20:41:59 +0100

    aaa

--- src/main/java/it/univaq/google/hashcode/impl/GreedySolvableImplLor.java
+++ src/main/java/it/univaq/google/hashcode/impl/GreedySolvableImplLor.java
@@ -25,6 +25,7 @@ import it.univaq.google.hashcode.model.ProblemInstance;
 import it.univaq.google.hashcode.model.Ride;
 import it.univaq.google.hashcode.model.Solution;
 import it.univaq.google.hashcode.model.Vehicle;
+import it.univaq.google.hashcode.model.VehicleDistance;
 import it.univaq.google.hashcode.util.ProblemUtil;
 
 public class GreedySolvableImplLor implements ISolvable {
@@ -55,12 +56,67 @@ public class GreedySolvableImplLor implements ISolvable {
 			}
 			
 			
+			//assegno i veicoli liberi alle rides disponibili
+			for(Ride ride : problemInstance.getRides()) {
+				List<VehicleDistance> veiDis = new ArrayList<VehicleDistance>();
+				for(Vehicle vei : veicoliLiberi) {
+					int distance = getDistanceTo(vei, ride.getStart());
+					VehicleDistance vd = new VehicleDistance(vei, distance, ride);
+					veiDis.add(vd);
+				}
+				
+				VehicleDistance vdMin = new VehicleDistance(null, Integer.MAX_VALUE, null);
+				for(VehicleDistance vd : veiDis) {
+					if(vd.distance < vdMin.distance) {//i have a new min
+						vdMin = vd;
+					}
+				}
+
+				//remove the ride from list
+				vdMin.vehicle.setNextRide(vdMin.ride);
+				problemInstance.getRides().remove(vdMin.ride);
+				
+			}
 			
-			for(Vehicle vei : veicoliLiberi) {
-				for(Ride ride : problemInstance.getRides()) {
+			for(Vehicle vei : veicoli) {
+				//se il veicolo ha una curRide o nextRide lo muovo di uno verso la rideEnd o rideStart, altrimenti?
+				if(vei.getCurrentRide() != null) {
+					//go to the end
+					vei.setCurrentPosition(getNextVeichleStep(vei, vei.getCurrentRide().getEnd()));
+				}
+				else if(vei.getNextRide() != null) {
+					//go to the start of this ride
+					vei.setCurrentPosition(getNextVeichleStep(vei, vei.getNextRide().getStart()));
+				}
+				else {
+					//TODO
 					
 				}
 			}
+			
+			//controllo se sono all inizio o alla fine di qualcosa
+			for(Vehicle vei : veicoli) {
+				if(vei.getCurrentRide() != null) {
+					if(vei.getCurrentPosition().x == vei.getCurrentRide().getEnd().x && vei.getCurrentPosition().y == vei.getCurrentRide().getEnd().y) {
+						//è finito il tragitto, il veicolo è libero
+						vei.getRideDone().add(vei.getCurrentRide());
+						vei.setCurrentRide(null);
+						vei.setNextRide(null);
+						
+					}
+				}
+				else if(vei.getNextRide() != null) {
+					if(vei.getCurrentPosition().x == vei.getCurrentRide().getStart().x && vei.getCurrentPosition().y == vei.getNextRide().getEnd().y) {
+						//è finito il tragitto, il veicolo è libero
+						vei.getRideDone().add(vei.getCurrentRide());
+						vei.setCurrentRide(null);
+						vei.setNextRide(null);
+						
+					}
+				}
+				
+			}
+
 			
 			
 		}
@@ -68,6 +124,22 @@ public class GreedySolvableImplLor implements ISolvable {
 		return new Solution(ProblemUtil.calculateScore());
 	}
 	
+	
+	public Coordinate getNextVeichleStep(Vehicle vei, Coordinate dest) {
+		Coordinate coord = vei.getCurrentPosition();
+		
+		if(vei.getCurrentPosition().x > dest.x)
+			coord.x -= 1;
+		else if(vei.getCurrentPosition().x < dest.x) 
+			coord.x += 1;
+		else if(vei.getCurrentPosition().y > dest.y) 
+			coord.y -= 1;
+		else if(vei.getCurrentPosition().y < dest.y) 
+			coord.y += 1;
+
+		return coord;
+		
+	}
 	
 	public Ride getBestRide(Vehicle vei, int curTime, ProblemInstance problemInstance) {
 		
